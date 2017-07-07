ec2bb13d479bda5a916661d793dc340fdd2fa46a
Ann Blodgett <w.annblodgett@gmail.com>
2017-07-07 13:44:15 -0600

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,10 @@
+//Write a program that prints a greeting of your choice, perhaps in a language other than English.
+
+
+public class HolaMundo{
+    public static void main(String[] args)
+   {	
+      System.out.println("Hola, Mundo!");
+ 	
+   }  	
+}
