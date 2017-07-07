b8d60792c6ee055b257e9d2537cb83a4a527f438
Ann Blodgett <w.annblodgett@gmail.com>
2017-07-07 13:55:02 -0600

    Add files via upload

---
+++ C169/PrintProduct.java
@@ -0,0 +1,13 @@
+//Write a program that prints the product of the first ten positive integers, 1 * 2 * … * 10.
+public class PrintProduct{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       System.out.println(1*2*3*4*5*6*7*8*9*10);
+        
+    }
+    
+}
